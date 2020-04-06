42fbfe64183c0e056ded7bc17147e62bc2eb5b8a
Nicole OBomsawin <nicoleobomsawin@gmail.com>
2020-04-06 12:10:37 -0400

    progress on search

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-$(window).on("load", function() {
-    var location = prompt("What's your zipcode?");
+
+var todaysDate;
+
+$("button").on("click", function(){
+    $("#currentLocation").empty();
+    var location = $("#searchBar").val();
 
     $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather?zip=" + location + "&appid=ee1f399344dd6005017bc6cdb6b32e3b&units=imperial",
+        url: "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=ee1f399344dd6005017bc6cdb6b32e3b&units=imperial",
         method: "GET"
     }).then(function(response) {
         var weatherID = response.weather[0].id;
@@ -22,22 +26,23 @@ $(window).on("load", function() {
             weatherIcon = 'http://openweathermap.org/img/wn/01d.png';
         // Cloudy
         } else if (weatherID >= 801 && weatherID <= 804) {
-            weatherIcon = 'http://openweathermap.org/img/wn/03d12309.png';
+            weatherIcon = 'http://openweathermap.org/img/wn/03d.png';
         } else {
 
         }
 
         var city = response.name;
-        var todaysDate = moment().format('MMMM Do, YYYY');
+
+        todaysDate = moment().format('MMMM Do, YYYY');
         $("#currentLocation").append("<h2>" + city + "<img src='" + weatherIcon + "'</h2>");
         $("#currentLocation").append("<b>" + todaysDate + "</b><br><br>");
 
         var temp = Math.round(response.main.temp);
         $("#currentLocation").append("Temperature: " + temp + "° F<br><br>");
-      
+        
         var humidity = response.main.humidity;
         $("#currentLocation").append("Humidity: " + humidity + "% <br><br>");
-      
+        
         var wind = response.wind.speed;
         $("#currentLocation").append("Wind Speed: " + wind + " MPH <br><br>");
 
@@ -52,17 +57,33 @@ $(window).on("load", function() {
             $("#currentLocation").append("UV Index: " + uv + "<br><br>");
         });
 
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/forecast?q=" + location + "&appid=ee1f399344dd6005017bc6cdb6b32e3b&units=imperial",
+            method: "GET"
+        }).then(function(response) {
+            $("#title").append("<h2>5-Day Forecast:</h2><br>")
+            for (i=0; i<response.list.length; i++) {
+                if (response.list[i].dt_txt.indexOf("12:00:00") !== -1) {
+                    var newCol = $("<div>").attr("class", "col-md-2 forecast");
+    
+                    var day = moment(response.list[i].dt, "X").format("MMMM Do");
+                    $(newCol).append(day + "<br><br>");
+
+                    var img = "http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon;
+                    $(newCol).append("<img src'" + img + "'>");
+
+                    var nextDayTemp = Math.round(response.list[i].main.temp);
+                    $(newCol).append("Temp: " + nextDayTemp + "° F<br>");
+        
+                    var nextDayHumidity = response.list[i].main.humidity;
+                    $(newCol).append("Humidity: " + nextDayHumidity + "%");
+        
+                    $("#fiveDay").append(newCol);
+                }
+            }
+        });
+    
+
     });
 
-    $.ajax({
-        url: "api.openweathermap.org/data/2.5/forecast?zip=" + location + ",us&appid=ee1f399344dd6005017bc6cdb6b32e3b",
-        method: "GET"
-    }).then(function(response) {
-        var day1 = moment().subtract(10, 'days').calendar();
-        var day2 = moment().add(1, 'days').calendar();
-
-        console.log(day1);
-        console.log(day2);
-    });
-
-});
+})
